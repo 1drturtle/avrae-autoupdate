d89714716562be14246581c4fb35442019fa5679
Chris A <1drturtle@gmail.com>
2021-01-25 00:59:49 -0500

    recursively generate file paths

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import os
+import pathlib
+import requests
 import json
+import collections
 
 
 class MissingArgument(BaseException):
     pass
 
+
+class RequestError(BaseException):
+    pass
+
+# utility functions
 
 def contains_ending(files, endings):
     return any([file.endswith(endings) for file in files])
@@ -17,10 +25,122 @@ def scan_directories(scan_path: str = None, collections={}):
         if not contains_ending(filenames, ('alias', 'snippet', 'gvar')):
             continue
         shared = os.path.commonprefix([dirpath, cwd])
-        dirname = dirpath.lstrip(shared).replace('\\', '/')
-        if dirname in collections:
-            active_dirs.append(dirpath.lstrip(shared))
+        if (dirname := dirpath.lstrip(shared).replace('\\', '/')) in collections:
+            active_dirs.append(dirname)
     return active_dirs
+
+
+def files_in_actives(scan_path, modified):
+    cwd = scan_path or os.getcwd()
+    active_files = []
+    for dirpath, dirnames, filenames in os.walk(cwd):
+        for file in filenames:
+            shared = os.path.commonprefix([cwd, dirpath])
+            relative = os.path.join(os.path.relpath(dirpath, cwd), file)
+            if '\\' in relative:
+                relative = pathlib.PureWindowsPath(relative)
+            else:
+                relative = pathlib.PurePosixPath(relative)
+            if (path := relative.as_posix()) in modified:
+                active_files.append(path)
+    return active_files
+
+
+# take a collection's data and path
+# and construct file paths based on
+# alias name + parent names as folders
+# if it's a base alias, there is an alias in it's own folder
+# alias with sub-aliases in folder, alias with no sub-aliases by itself
+
+ConstructedPath = collections.namedtuple('ConstructedPath', ['rel_path'])
+
+
+def construct_file_paths(collection_data, collection_path):
+    logged_parents = {}
+    file_paths = []
+
+    def parse_alias(alias_data):
+        folder_name = alias_data['name'].lower()
+        curpath = None
+        if alias_data['parent_id']:
+            curpath = os.path.join(logged_parents[alias_data['parent_id']], folder_name)
+        else:
+            curpath = os.path.join(collection_path, folder_name)
+
+        if '\\' in curpath:
+            curpath = pathlib.PureWindowsPath(curpath)
+        else:
+            curpath = pathlib.PurePosixPath(curpath)
+        curpath = curpath.as_posix()
+
+        logged_parents[alias_data['_id']] = curpath
+        file_paths.append(curpath + '.alias')
+        for subalias in alias_data['subcommands']:
+            parse_alias(subalias)
+
+    for alias in collection_data['aliases']:
+        parse_alias(alias)
+
+    return file_paths
+
+
+# Requests
+
+def post_request(api_key, path, data):
+    headers = {
+        'Authorization': api_key
+    }
+    r = requests.post(
+        url=path,
+        headers=headers,
+        json=data
+    )
+    return r.json()
+
+
+def put_request(api_key, path, data):
+    headers = {
+        'Authorization': api_key
+    }
+    r = requests.put(
+        url=path,
+        headers=headers,
+        json=data
+    )
+    return r.json()
+
+
+def get_collection_info(api_key, collection_id):
+    path = f'https://api.avrae.io/workshop/collection/{collection_id}/full'
+    headers = {
+        'Authorization': api_key
+    }
+    r = requests.get(
+        url=path,
+        headers=headers
+    )
+    data = r.json()
+    if not data['success']:
+        raise RequestError(f'{collection_id} collection data grab did not succeed.\n{json.dumps(data, indent=4)}')
+    return r.json()
+
+
+def update_workshop_obj(type: str, object_id, code: str, api_key):
+    if type not in ['alias', 'snippet']:
+        raise ValueError('Type must be alias or snippet')
+    url = f'https://api.avrae.io/workshop/{type}/{object_id}/code'
+    data = {
+        'content': code
+    }
+    code_version = post_request(api_key, url, data)
+    if not code_version['success']:
+        raise RequestError(f'{type.title()} Code Version did not succeed.\n{json.dumps(code_version, indent=4)}')
+    put_url = f'https://api.avrae.io/workshop/{type}/{object_id}/active-code'
+    put_data = {
+        'version': code_version['data']['version']
+    }
+    set_active = put_request(api_key, put_url, put_data)
+    return set_active
 
 
 if __name__ == '__main__':
@@ -37,4 +157,18 @@ if __name__ == '__main__':
     if AVRAE_TOKEN is None:
         raise MissingArgument('No Avrae Token Found.')
 
+    # Grab directories that have files that end in .alias, .snippet, or .gvar
     active_directories = scan_directories(repo_path, collection_ids)
+
+    # Go through our modified files and see if they are in active directories
+    active_files = files_in_actives(repo_path, modified_files_list)
+
+    # construct possible file paths from collections
+    for collection in collection_ids:
+        data = get_collection_info(AVRAE_TOKEN, collection_ids[collection])
+        paths = construct_file_paths(data['data'], collection)
+        print(paths, modified_files_list)
+
+    # Get Collection Data
+    # Update Aliases/Snippets
+    # Update GVAR
